2551c70cc816edc285b9af7ae8645b57393c5f82
Spaske99 <nenad.spasic.99@gmail.com>
2024-05-20 12:08:31 +0200

    Finalize Investment Calculator App
    Complete setup with user input handling, zero value validation, and results calculation. The App manages main logic and state, UserInput handles input fields, and Results displays calculated data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import Header from "./components/Header";
+import Results from "./components/Results";
+import UserInput from "./components/UserInput";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifire, newValue) {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputIdentifire]: +newValue,
+      };
+    });
+  }
+
+  let hasZeroValue = false;
+
+  for (let key in userInput) {
+    if (userInput[key] <= 0) {
+      hasZeroValue = true;
+      break;
+    }
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <main>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {hasZeroValue && (
+        <p className="center">Please enter values greater than zero.</p>
+      )}
+      {!hasZeroValue && <Results userInput={userInput} />}
+    </main>
+  );
 }
 
-export default App
+export default App;
